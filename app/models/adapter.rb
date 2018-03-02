64c4467c3906bebc065dc040c2739e142ab2f814
github username <github email address>
2018-03-02 10:50:23 -0500

    Done.

--- app/models/adapter.rb
+++ app/models/adapter.rb
@@ -1,7 +1,8 @@
 require 'pry'
 require 'json'
 
-# In terminal, you should be able to call Adapter.new('newyorker.json').create_objects_from_file and get the created Article objects as a return value
+# In terminal, you should be able to call Adapter.new('newyorker.json').create_objects_from_file
+# and get the created Article objects as a return value
 
 class Adapter
   attr_accessor :file, :articles
@@ -17,6 +18,13 @@ class Adapter
 
   def create_objects_from_file
     # create article and category objects here
+    # This method should use the provided articles data to create Article and Category
+    # objects. It should return the created instances.
+    Adapter.articles(file).map do |a|
+      Article.new(a["contributor"], a["title"], a["description"], a["url"], a["publishedAt"])
+      Category.new(a["category"])
+    end
+
   end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,32 @@
 class Article
 
+  attr_accessor :title, :description, :url, :contributor, :publishedAt
 
+  @@all = []
+
+  def initialize(contributor, title, description, url, publishedAt)
+    @title = title
+    @description = description
+    @url = url
+    @contributor = contributor
+    @publishedAt = publishedAt
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_category(category)
+    self.category = category
+  end
+
+  def self.find_all_by_category(category)
+    # Takes a category instance as an argument, and returns all articles associated
+    # with that category
+
+    # categories = Category.all.select {|c| c == category}
+    Article.all.select {|a| a.category = category}
+  end
 
 end
